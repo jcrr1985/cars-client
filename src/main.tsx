0953380ac6537fe5a95e870c4804dee63856fdc6
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-06-12 19:00:08 +0500

    fixing apiUrl

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-export const apiUrl =
-  "cars-server-3cb4-1882vydic-jcrr1985s-projects.vercel.app";
+export const apiUrl = "https://cars-server-3cb4.vercel.app/";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
