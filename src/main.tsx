60844449d11f7c74372a94812c271b12f044917f
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-06-13 05:28:01 +0500

    changing ApiUrl

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-export const apiUrl = "https://cars-server-3cb4.vercel.app";
+export const apiUrl = "https://cars-server-production.up.railway.app/";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
