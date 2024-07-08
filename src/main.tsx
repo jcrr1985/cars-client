4cde77f524331f5eb634f69e44eca6bbbde0ad4f
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-07-09 04:21:51 +0500

    changing apiUrl from Render to Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-export const apiUrl = "https://cars-server-production.up.railway.app";
+export const apiUrl =
+  "https://cars-server-f6zz898tq-jcrr1985s-projects.vercel.app/";
 // export const apiUrl = "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
