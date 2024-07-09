78659450a47a478baf78213b773799c9954a537b
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-07-09 05:43:13 +0500

    removing extra slash

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 export const apiUrl =
-  "https://cars-server-o9l6wrigq-jcrr1985s-projects.vercel.app/";
+  "https://cars-server-o9l6wrigq-jcrr1985s-projects.vercel.app";
 // export const apiUrl = "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
