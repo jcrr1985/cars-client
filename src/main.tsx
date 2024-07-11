d86911926d3b8e88396881bc9c7f3a5e549d6f5e
julio rodriguez <julio.rodriguezr1@gmail.com>
2024-07-11 22:41:05 +0500

    changing apiUrl to RTH's one

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-export const apiUrl =
-  "https://cars-server-o9l6wrigq-jcrr1985s-projects.vercel.app";
+export const apiUrl = "https://rth-server.vercel.app";
 // export const apiUrl = "http://localhost:3001";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
